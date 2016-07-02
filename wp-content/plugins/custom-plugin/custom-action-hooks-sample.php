55895e23db1ba861f602eb728fc57bfd71261531
naveen <naveen.goud@karmanya.co.in>
2016-07-02 19:09:47 +0530

    my wordpree

--- wp-content/plugins/custom-plugin/custom-action-hooks-sample.php
+++ wp-content/plugins/custom-plugin/custom-action-hooks-sample.php
@@ -18,3 +18,22 @@ function dwwp_addaction_hooks() {
 }
 
 add_action('wp_before_admin_bar_render', 'dwwp_addaction_hooks');
+
+function dwwp_action_hooks_hooks() {
+    global $wp_admin_bar;
+    //var_dump($wp_admin_bar);
+    $wp_admin_bar->add_menu(array(
+        'id' => 'my menu',
+        'title' => 'MY Menu',
+        'href' => 'https://www.google.com'
+    ));
+}
+
+add_action('wp_before_admin_bar_render', 'dwwp_action_hooks_hooks');
+
+function dwwp_change_icon($plural) {
+    $plural['name'] = 'naveen';
+    return $args;
+}
+
+add_filter('dwwp_post_type_args', 'dwwp_change_icon');
--- wp-content/plugins/custom-plugin/custom-create-shortcode.php
+++ wp-content/plugins/custom-plugin/custom-create-shortcode.php
@@ -14,3 +14,19 @@ function dwwp_custome_shortcode_callback($atts, $content = NULL) {
 }
 
 add_shortcode('custom_shortcode', 'dwwp_custome_shortcode_callback');
+
+function dwwp_shortcode_callback($atts, $content = NULL) {
+    //print_r($atts);
+    $atts = shortcode_atts(
+            array(
+        'title' => 'My Title',
+        'src' => 'ww.google.com'
+            ), $atts
+    );
+    
+    return '<h1>' . $atts['title'] . '</h1>';
+}
+
+add_shortcode('custom_shortcode', 'dwwp_shortcode_callback');
+
+
